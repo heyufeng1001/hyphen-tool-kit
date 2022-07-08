4ebed3208acad4fc6d56698719705bc13ac61beb
heyufeng.1001 <heyufeng.1001@bytedance.com>
2022-07-08 17:36:38 +0800

    feat：新增三元操作函数
    Signed-off-by: heyufeng.1001 <heyufeng.1001@bytedance.com>

--- util/util.go
+++ util/util.go
@@ -3,3 +3,17 @@
 // Copyright 2022 hyphen. All rights reserved.
 // create-time: 2022/7/8
 package util
+
+func TernaryForm[T any](condition bool, trueVal, falseVal T) T {
+	if condition {
+		return trueVal
+	}
+	return falseVal
+}
+
+func TernaryFormInterface(condition bool, trueVal, falseVal interface{}) interface{} {
+	if condition {
+		return trueVal
+	}
+	return falseVal
+}
